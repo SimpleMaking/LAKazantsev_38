d0498ab0c041675135188b6a0c7dda651067cdef
Leo Kazantsev <leokazantsev19@mail.ru>
2022-09-19 11:19:47 +0300

    modified main.cpp

--- Lab_1/main.cpp
+++ Lab_1/main.cpp
@@ -3,8 +3,14 @@
 using namespace std;
 
 
-vector<float> func_eratosf(int n, float* curr_arr, vector<float> ready_arr)
+void func_eratosf(int n)
 {
+	vector<float> ready_arr;
+	float* curr_arr = new float[n];
+
+	for (size_t i = 0; i <= n; ++i)  // formation of the initial arr  
+		curr_arr[i] = i;
+
 	size_t i = 2, j = 0;
 	while (i < pow(n, 0.5))         // iterate up to half of the original arr by the first iterator
 	{
@@ -19,32 +25,23 @@ vector<float> func_eratosf(int n, float* curr_arr, vector<float> ready_arr)
 			}
 		}
 		i = i + 1;
-
 	}
+
 	for (size_t i = 0; i < n; ++i) 
 		if (curr_arr[i] != 0)
 			ready_arr.push_back(curr_arr[i]);
-	
-	return ready_arr;
+
+	for (size_t i = 0; i < ready_arr.size(); ++i) // ready data output
+		cout << ready_arr[i] << " ";
+
+	ready_arr.clear();
 }
 
 
 int main(void)
 {
-	vector<float> ready_arr;
-	short n = 9; // count of numbers in the original arr
+	short n = 9;  // count of numbers in the original arr
+	func_eratosf(n);  // application of the algorithm                                 
 
-	float* curr_arr = new float[n];
-	for (size_t i = 0; i <= n; ++i)  // formation of the initial arr  
-		curr_arr[i] = i;
-
-	curr_arr[1] = 0;              // application of the algorithm                                 
-	ready_arr = func_eratosf(n, curr_arr, ready_arr); 
-
-	for (size_t i = 0; i < ready_arr.size(); ++i) // ready data output
-		cout << ready_arr[i] << " ";
-    
-	ready_arr.clear();
-	
 	return 0;
 }
